f0ac429e97cdfb08f3d83d5e0f6b91a63dc36841
TusharKanaujia <tushar.kanaujia@gmail.com>
2020-03-22 17:58:38 +0000

    Section 3 end

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
                 optionsAction.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             }
             );
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -44,7 +51,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
