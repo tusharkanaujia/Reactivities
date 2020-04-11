721e2c754dccb606a79ccb92523867fe3c27b7c8
TusharKanaujia <tushar.kanaujia@gmail.com>
2020-04-11 18:11:25 +0100

    EOD Sec 5

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -8,40 +8,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistance;
 
-namespace Application.Activities
-{
-    public class List
-    {
-        public class Query : IRequest<List<Activity>>{}
+namespace Application.Activities {
+    public class List {
+        public class Query : IRequest<List<Activity>> { }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
-        {
+        public class Handler : IRequestHandler<Query, List<Activity>> {
             private readonly DataContext _context;
 
-            public Handler(DataContext context, ILogger<List> logger)
-            {
-                _context=context;
+            public Handler (DataContext context, ILogger<List> logger) {
+                _context = context;
                 _logger = logger;
             }
 
             public ILogger<List> _logger { get; }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                try
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000,cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
+            public async Task<List<Activity>> Handle (Query request, CancellationToken cancellationToken) {
+                try {
+
+                    cancellationToken.ThrowIfCancellationRequested ();
+                    await Task.Delay (1, cancellationToken);
+                    _logger.LogInformation ($"Task has completed");
+
+                } catch (Exception ex) {
+                    _logger.LogInformation ($"Task cancelled");
                 }
-                catch (Exception ex) 
-                {
-                    _logger.LogInformation($"Task cancelled");
-                }
-                var activities = await _context.Activities.ToListAsync(cancellationToken);
+                var activities = await _context.Activities.ToListAsync (cancellationToken);
                 return activities;
             }
         }
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,106 @@
+import React, { useState, FormEvent } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import {v4 as uuid} from 'uuid'
+
+interface IProps {
+  setEditMode: (editMode: boolean) => void;
+  activity: IActivity;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+}
+
+export const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormState,
+  createActivity,
+  editActivity
+}) => {
+  const initializeForm = () => {
+    if (initialFormState) {
+      return initialFormState;
+    } else {
+      return {
+        id: "",
+        title: "",
+        description: "",
+        category: "",
+        date: "",
+        city: "",
+        venue: ""
+      };
+    }
+  };
+
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+
+  const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.currentTarget;
+    setActivity({ ...activity, [name]: value });
+  };
+
+  const handleSubmit= ()=>{
+    if (activity.id.length===0) {
+      let newActivity ={
+        ...activity,
+        id: uuid()
+      }
+      createActivity(newActivity);
+    }
+    else{
+      editActivity(activity);
+    }
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input
+          placeholder="Title"
+          onChange={handleInputChange}
+          name="title"
+          value={activity.title}
+        />
+        <Form.TextArea
+          rows={2}
+          placeholder="Description"
+          onChange={handleInputChange}
+          name="description"
+          value={activity.description}
+        />
+        <Form.Input
+          placeholder="Catagory"
+          onChange={handleInputChange}
+          name="category"
+          value={activity.category}
+        />
+        <Form.Input
+          type="datetime-local"
+          placeholder="Date"
+          onChange={handleInputChange}
+          name="date"
+          value={activity.date}
+        />
+        <Form.Input
+          placeholder="City"
+          onChange={handleInputChange}
+          name="city"
+          value={activity.city}
+        />
+        <Form.Input
+          placeholder="Vanve"
+          onChange={handleInputChange}
+          name="venue"
+          value={activity.venue}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          floated="right"
+          type="submit"
+          content="Cancel"
+          onClick={() => setEditMode(false)}
+        />
+      </Form>
+    </Segment>
+  );
+};
